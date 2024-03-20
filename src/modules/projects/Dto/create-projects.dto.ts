e755c87734b4570f5daba12f9366b12a9d2a6311
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-03-20 14:58:19 -0300

    feat: add character validation

--- src/modules/projects/Dto/create-projects.dto.ts
+++ src/modules/projects/Dto/create-projects.dto.ts
@@ -1,7 +1,19 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class CreateProjectsDto {
+  @IsString()
   title: string;
+
+  @IsString()
+  @IsOptional()
   image: string | null;
+
+  @IsString()
   url: string;
+
+  @IsString()
   github: string;
+
+  @IsString()
   description: string;
 }
