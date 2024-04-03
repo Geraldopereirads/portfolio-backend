1236d2623618e50d60987262bec1184764275c63
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-03 12:13:12 -0300

    feat: add crud on module

--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -1,16 +1,19 @@
-import { Module } from "@nestjs/common";
-import { ProjectsController } from "./projects.controller";
-import { ProjectsService } from "./projects.service";
-import { ProjectsRepository } from "./repositories/projects.repository";
+import {Module} from '@nestjs/common';
+import {ProjectsController} from './projects.controller';
+import {ProjectsService} from './projects.service';
+import {ProjectsRepository} from './repositories/projects.repository';
+import {ProjectsPrismaRepository} from './repositories/prisma/projects-prisma.repository';
+import {PrismaService} from '../database/prisma.service';
 
 @Module({
-    controllers: [ProjectsController],
-    providers: [
-        ProjectsService
+  controllers: [ProjectsController],
+  providers: [
+    ProjectsService,
+    PrismaService,
     {
-            provide: ProjectsRepository,
-            useClass:
-    }
-    ]
+      provide: ProjectsRepository,
+      useClass: ProjectsPrismaRepository,
+    },
+  ],
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
