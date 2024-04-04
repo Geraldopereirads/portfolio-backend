5c6234df2b6ec5df422cd72b86799e729df0fee0
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-04 10:26:52 -0300

    feat: add entities frontend and backend

--- src/modules/backend/entities/backend.entity.ts
+++ src/modules/backend/entities/backend.entity.ts
@@ -1,1 +1,14 @@
-export class Backend {}
+import {randomUUID} from 'crypto';
+
+export class Backend {
+  readonly id: string;
+  title: string;
+  image: string | null;
+  url: string;
+  github: string;
+  description: string;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
--- src/modules/frontend/entities/frontend.entity.ts
+++ src/modules/frontend/entities/frontend.entity.ts
@@ -1,1 +1,14 @@
-export class Frontend {}
+import {randomUUID} from 'crypto';
+
+export class Frontend {
+  readonly id: string;
+  title: string;
+  image: string | null;
+  url: string;
+  github: string;
+  description: string;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
