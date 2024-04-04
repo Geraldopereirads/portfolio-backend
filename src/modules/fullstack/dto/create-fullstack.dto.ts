b1aa9c3e9677124ee6cb28c2a8cf21a74b6fdceb
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-04 17:28:54 -0300

    feat: add dto fullstack

--- src/modules/fullstack/dto/create-fullstack.dto.ts
+++ src/modules/fullstack/dto/create-fullstack.dto.ts
@@ -1,1 +1,23 @@
-export class CreateFullstackDto {}
+import {IsString, IsNotEmpty, IsOptional} from 'class-validator';
+
+export class CreateFullstackDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  image: string | null;
+
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  github: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
