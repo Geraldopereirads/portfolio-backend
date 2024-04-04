c3e869e7dc0aa0d430b41bcf32c05f756e6a0224
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-04 11:03:20 -0300

    feat: add DTO frontend and backend

--- src/modules/backend/dto/create-backend.dto.ts
+++ src/modules/backend/dto/create-backend.dto.ts
@@ -1,1 +1,23 @@
-export class CreateBackendDto {}
+import {IsString, IsNotEmpty, IsOptional} from 'class-validator';
+
+export class CreateBackendDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  image: string | null;
+
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  github: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
