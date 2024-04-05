3a23f2785e6ff7c2f9b5663611c0742db524cdc4
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-05 10:41:23 -0300

    fix: repair main and app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import {Module} from '@nestjs/common';
 import {ProjectsModule} from './modules/projects/projects.module';
-import { BackendModule } from './modules/backend/backend.module';
-import { FrontendModule } from './modules/frontend/frontend.module';
-import { FullstackModule } from './modules/fullstack/fullstack.module';
+import {BackendModule} from './modules/backend/backend.module';
+import {FrontendModule} from './modules/frontend/frontend.module';
+import {FullstackModule} from './modules/fullstack/fullstack.module';
 @Module({
   imports: [ProjectsModule, BackendModule, FrontendModule, FullstackModule],
   controllers: [],
