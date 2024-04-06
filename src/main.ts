80f8ccc811c9078b82711b3490f12e49dbc04d5a
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-04-06 08:59:42 -0300

    feat: config swagger on main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from '@nestjs/common';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Portfólio app')
+    .setDescription('Listen your projects')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   await app.listen(3002);
 }
