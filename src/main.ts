12dc246629b41a6a35b115797a5d4a8261312783
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-06-05 20:38:24 -0300

    fix: repair package.json

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Portfólio app')
     .setDescription('Listen your projects')
